eb6a3d93f07161c96995e14e5ba11b3e62c1316f
Кудина Вероника Евгеньевна <kvckey@niuitmo.ru>
2025-11-29 14:53:04 +0300

    add init method to generate 4x4 grid dynamically

--- game.js
+++ game.js
@@ -5,7 +5,22 @@ class Game2048 {
         this.score = 0;
         this.gameOver = false;
         this.history = [];
+
+        this.init();
     }
+
+    init() {
+    const gameBoard = document.getElementById('gameBoard');
+    gameBoard.innerHTML = '';
+    
+    for (let i = 0; i < this.size * this.size; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        gameBoard.appendChild(cell);
+    }
+    
+    this.resetGame();
+}
 }
 
 const game = new Game2048();
