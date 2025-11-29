691242e37ca188511018cf2994dc597104c061bb
Кудина Вероника Евгеньевна <kvckey@niuitmo.ru>
2025-11-29 14:49:35 +0300

    create Game2048 class with basic properties

---
+++ game.js
@@ -0,0 +1,11 @@
+class Game2048 {
+    constructor() {
+        this.size = 4;
+        this.board = [];
+        this.score = 0;
+        this.gameOver = false;
+        this.history = [];
+    }
+}
+
+const game = new Game2048();
